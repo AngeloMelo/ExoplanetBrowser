8f8513667a0087f34333903d8cac57e150c953e3
AngeloMelo <angelo.s.melo@gmail.com>
2021-02-05 14:31:22 -0300

    deployment configs

--- server.js
+++ server.js
@@ -1,39 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const discMethodRoutes = require('./routes/discmethods');
-const facilityRoutes = require('./routes/facilites');
-const hostRoutes = require('./routes/host');
-const planetRoutes = require('./routes/planets');
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const discMethodRoutes = require('./routes/discmethods')
+const facilityRoutes = require('./routes/facilites')
+const hostRoutes = require('./routes/host')
+const planetRoutes = require('./routes/planets')
 const path = require('path')
-const cron = require('node-cron');
+const cron = require('node-cron')
 const ReloadDbTask = require('./tasks/reloadDbTask')
 
-const app = express();
-app.use(bodyParser.json()); 
+const app = express()
+app.use(bodyParser.json())
 
-const dbUrl = require('./config/keys').mongoURI;
+let dbUrl = require('./config/keys').mongoURI
+
+if(process.env.NODE_ENV ===	'production' )
+{
+	dbUrl = process.env.DATABASE_URL
+}
 
 mongoose
 	.connect(dbUrl,{useNewUrlParser: true})
 	.then(()=> console.log('mongoDb connected.'))
-	.catch(err => console.log(err));
+	.catch(err => console.log(err))
 
-app.use('/api/discmethods', discMethodRoutes);
-app.use('/api/facilites', facilityRoutes);
-app.use('/api/hosts', hostRoutes);
-app.use('/api/planets', planetRoutes);
+app.use('/api/discmethods', discMethodRoutes)
+app.use('/api/facilites', facilityRoutes)
+app.use('/api/hosts', hostRoutes)
+app.use('/api/planets', planetRoutes)
 
 if(process.env.NODE_ENV ===	'production' )
 {
-	app.use(express.static('frontend/build'));
+	app.use(express.static('frontend/build'))
+
 	app.get('*', (req, res)=>{
 		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-	});
+	})
 }	
 
-cron.schedule('* * * *', ReloadDbTask);
+cron.schedule('* * * *', ReloadDbTask)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server is listenning on ${port}`));
+app.listen(port, () => console.log(`Server is listenning on ${port}`))
